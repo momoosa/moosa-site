87cb82eb40c042abd30e73d8b43ee2489fad6dd5
Mo Moosa <momoosa@me.com>
2026-05-06 19:02:42 +0200

    Content

--- moosa/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/triple-slash-reference
-/// <reference path="../.astro/types.d.ts" />
-
-interface Window {
-  __themeSwapInit?: boolean;
-  __themeToggleInit?: boolean;
-}
